dea8bf4a32dbad58f27be249bc15362f11b66c3b
helenavprado <helena.vprado@hotmail.com>
2022-10-10 17:38:19 -0700

    multiple colors version -not working

--- color-picker/src/atoms/ColorName.jsx
+++ color-picker/src/atoms/ColorName.jsx
@@ -4,7 +4,7 @@ import "../style.css";
 class ColorName extends React.Component {
     render() {
         return (
-            <h2>This color is rbg</h2>
+            <h2>your color is rbg({this.props.rgb1}, {this.props.rgb2}, {this.props.rgb3})</h2>
         );
     }
 }
--- color-picker/src/molecules/RandomColor.jsx
+++ color-picker/src/molecules/RandomColor.jsx
@@ -7,32 +7,49 @@ class RandomColor extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            color: "green",
+            rgb1: 255,
+            rgb2: 0,
+            rgb3: 90
         }
         this.handleClick = this.handleClick.bind(this)
+        this.formatColor = this.formatColor(this)
+
     }
 
-    // generateRbg() {
-    //     const number = Math.floor(Math.random() * 256);
-    //     return number
-    // }
+    generatergb() {
+            return Math.floor(Math.random() * 256);
+    }
 
     handleClick() {
-        const newColor = this.state.color === "green" ? "yellow" : "green";
-        this.setState({color: newColor})
+        this.setState({
+            rgb1: this.generatergb(),
+            rgb2: this.generatergb(),
+            rgb3: this.generatergb(),
+        })
     }
 
+    formatColor() {
+        return `'rgb(${this.state.rgb1}, ${this.state.rgb2}, ${this.state.rgb3})'`
+    }
+
+    
 
     render() {
         return (
             <div className="randomcolormolecule">
-                <ColorName ></ColorName>
+                <ColorName rgb1={this.state.rgb1} rgb2={this.state.rgb2} rgb3={this.state.rgb3}></ColorName>
+                {/* por alguma razao o handleClick nao aceita parenteses na linha abaixo. Da muitos erros no console no browser */}
                 <Button clickProp={this.handleClick}></Button>
-                <div style={{backgroundColor: this.state.color}} className="square" >
+                <div style={{backgroundColor: 'rgb(30, 0, 99)'}} className="square" >
                 </div>
+                <h1>formartColor: {this.formatColor}</h1>
             </div>
             
         );
+    }
+
+    componentDidMount() {
+    
     }
 }
 
